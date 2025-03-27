37520bf9889d76194bda6046d54a56a0f416e58e
Kiiyo <hiimkaze@gmail.com>
2025-03-27 17:27:13 +0800

    move

--- main.py
+++ main.py
@@ -9,26 +9,24 @@ def main():
     y=SCREEN_HEIGHT/2
     player = Player(x,y,PLAYER_RADIUS)
     
-
-
-    print(f"Starting Asteroids!\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}\n")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     pygame.display.set_caption("Asteroids")
     pygame.init()
     while True:
-        screen.fill("black")
-
-        player.update(dt)
-        player.draw(screen)
-        player.move(PLAYER_TURN_SPEED * dt)
-        
-        pygame.display.flip()
-        dt = fps.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 exit()
 
+
+        player.update(dt)
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        
+        dt = fps.tick(60) / 1000
+        
+    
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 from circleshape import *
+from constants import *
 import pygame
 
 class Player(CircleShape):
@@ -19,17 +20,21 @@ class Player(CircleShape):
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
 
-    def rotate(self, angle):
-        self.rotation += angle
-
-    def move(self, dt):
+    def update(self, dt):
         keys = pygame.key.get_pressed()
 
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
-        # if keys[pygame.K_w]:
-        #     self.y -= self.speed
-        # if keys[pygame.K_s]:
-        #     self.y += self.speed
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
